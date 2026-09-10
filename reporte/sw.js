1077016e05d157a0f994499835816c8f75f0437e
tygserviciosveterinarios <tygserviciosveterinarios@gmail.com>
2026-09-10 17:11:24 -0300

    Delete reporte/sw.js

--- reporte/sw.js
+++
@@ -1,4 +0,0 @@
-// Service worker mínimo: no cachea nada especial, solo habilita la instalación como app.
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", (e) => e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch", () => {});
